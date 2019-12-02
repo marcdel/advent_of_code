bee2beaef28f8a7acdad0afa36082991382eff85
Marc Delagrammatikas <delagrammatikas@gmail.com>
2019-12-01 16:40:06 -0800

    Day 1, Part 2

--- 2019/day1.exs
+++ 2019/day1.exs
@@ -1,6 +1,15 @@
 defmodule Day1 do
+  def fuel_counter(mass) when mass < 0, do: 0
+
   def fuel_counter(mass) do
-    div(mass, 3) - 2
+    fuel = div(mass, 3) - 2
+    additional_fuel = fuel_counter(fuel)
+
+    if (additional_fuel >= 0) do
+      fuel + additional_fuel
+    else
+      fuel
+    end
   end
 
   def total_fuel_consumption(masses) do
