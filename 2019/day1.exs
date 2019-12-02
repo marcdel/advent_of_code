011db3da6909d2f3fc0c032769710540ea43be8f
Marc Delagrammatikas <delagrammatikas@gmail.com>
2019-12-01 17:06:50 -0800

    Day 1, Part 2: prefer pattern matching to if statements

--- 2019/day1.exs
+++ 2019/day1.exs
@@ -3,18 +3,18 @@ defmodule Day1 do
 
   def fuel_counter(mass) do
     fuel = div(mass, 3) - 2
-    additional_fuel = fuel_counter(fuel)
-
-    if (additional_fuel >= 0) do
-      fuel + additional_fuel
-    else
-      fuel
-    end
+    add_additional_fuel(fuel, fuel_counter(fuel))
   end
 
   def total_fuel_consumption(masses) do
     masses
     |> Enum.map(&fuel_counter/1)
     |> Enum.sum()
+  end
+
+  defp add_additional_fuel(fuel, additional_fuel) when additional_fuel < 0, do: fuel
+
+  defp add_additional_fuel(fuel, additional_fuel) do
+    fuel + additional_fuel
   end
 end
